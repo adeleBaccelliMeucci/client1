b5b3ced39291506ad325f4426dd1ddf87e9fca89
lai2-24 <lai2-24@itismeucci.com>
2024-10-11 09:23:14 +0200

    funzionano

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,6 +1,9 @@
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
@@ -8,6 +11,17 @@ public class Main {
     public static void main(String[] args) throws UnknownHostException, IOException {
         System.out.println("client partito");
         Socket s = new Socket("localhost", 3000);
-        System.out.println("il client è partito");
+        System.out.println("il client è collegato");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        DataOutputStream out = new DataOutputStream(s.getOutputStream());
+
+        String stringaInviata = "sdkfahfkg";
+        out.writeBytes(stringaInviata + "\n"); //manda
+
+        String stringaModificata = in.readLine(); //legge
+        System.out.println("stringa modificata: " + stringaModificata);
+
+        s.close();
     }
 }
